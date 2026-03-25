e16bee26c7163d1deb32312214339c6b077194ac
Kacper Rączy <gfw.kra@gmail.com>
2026-03-25 14:39:02 -0700

    Multiply

--- selfdrive/modeld/modeld.py
+++ selfdrive/modeld/modeld.py
@@ -388,7 +388,7 @@ def main(demo=False):
       drivingdata_send = messaging.new_message('drivingModelData')
       posenet_send = messaging.new_message('cameraOdometry')
 
-      frame_delay = modelv2_send.logMonoTime -  meta_main.timestamp_eof
+      frame_delay = (modelv2_send.logMonoTime -  meta_main.timestamp_eof) * 1e-9
       action = get_action_from_model(model_output, prev_action, lat_delay + frame_delay + DT_MDL, long_delay + DT_MDL, v_ego)
       prev_action = action
       fill_model_msg(drivingdata_send, modelv2_send, model_output, action,
